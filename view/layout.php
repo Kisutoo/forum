0f07527b99078d3eecff133f2d26c14993a754a3
Malkom <malk-om@outlook.fr>
2025-06-14 17:18:11 +0200

    Mise en place du site

---
+++ view/layout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <title>Forum</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php?ctrl=forum&action=listTopics">ListTopics</a>
+        </nav>
+    </header>
+    <main>
+        <?= $page ?>
+    </main>
+    
+</body>
+</html>
