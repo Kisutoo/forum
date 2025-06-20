c5795b25595551d495f8b92bec9917e8d996ca78
kisutoo <malk-om@outlook.fr>
2025-06-20 15:45:03 +0200

    .

--- view/forum/detailTopic.php
+++ view/forum/detailTopic.php
@@ -2,32 +2,34 @@
     $topic = $result["data"]["topic"];
     $posts = $result["data"]["posts"];
 ?>
-<div class="allPosts">
-<?php if($posts)
-    {
-        foreach($posts as $post){?>
-        <div class="message">
-            <p class="auteurPost"><?= $post->getUser()->getNickName()?></p>
-            <p><?= $post->getTexte() ?></p>
-        </div>
-    <?php } 
-    }?>
-</div>
-<?php if(App\Session::getUser())
-{ ?>
-<div class="addMessage">
-    <form class="addMessageForm" action="index.php?ctrl=topic&action=addMessage" method="POST">
-    <div class="section">
-        <label for="Message" aria-label="Ajouter un message au topic"></label>
-        <textarea type="text" name="texte" required="required" rows="3" cols="100" placeholder="Faites vous plaisir !"></textarea>
+<div class="allPostsContent">
+    <div class="allPosts">
+    <?php if($posts)
+        {
+            foreach($posts as $post){?>
+            <div class="message">
+                <p class="auteurPost"><?= $post->getUser()->getNickName()?></p>
+                <p><?= $post->getTexte() ?></p>
+            </div>
+        <?php } 
+        }?>
     </div>
-        <input class="envoyer" type="submit" name="submit" value="Envoyer">
-    </form>
+    <?php if(App\Session::getUser())
+    { ?>
+    <div class="addMessage">
+        <form class="addMessageForm" action="index.php?ctrl=topic&action=addMessage" method="POST">
+        <div class="section">
+            <label for="Message" aria-label="Ajouter un message au topic"></label>
+            <textarea type="text" name="texte" required="required" rows="3" cols="100" placeholder="Faites vous plaisir !"></textarea>
+        </div>
+            <input class="envoyer" type="submit" name="submit" value="Envoyer">
+        </form>
+    </div>
+    <?php } ?>
+    <?php if(App\Session::isAdmin())
+    { ?>
+    <div class="topics addCategorie">
+        <a class="envoyer" href="index.php?ctrl=topic&action=deleteTopic&id=<?= $topic->getId() ?>">Supprimer le topic</a>
+    </div>
+    <?php } ?>
 </div>
-<?php } ?>
-<?php if(App\Session::isAdmin())
-{ ?>
-<div class="topics addCategorie">
-    <a class="envoyer" href="index.php?ctrl=topic&action=deleteTopic&id=<?= $topic->getId() ?>">Supprimer le topic</a>
-</div>
-<?php } ?>
